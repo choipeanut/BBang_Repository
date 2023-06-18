b970cffe85ff3640a2d3c382ad7cfd4d73db0cb9
choipeanut <choipeanut09@gmail.com>
2023-06-18 12:40:24 +0900

    update enemy, boss and gun detail

--- pc.c
+++ pc.c
@@ -1104,3 +1104,992 @@ void CreateBullet(COORD Bullet) {
 		}
 	}
 }
+void Gun1Print(int i) {
+
+	setColor(14, 0);
+	printf("○");
+	setColor(15, 0);
+
+}
+void Gun2Print(int i) {
+
+	setColor(10, 0);
+	if (BULLET[i].BulletDirect == 0 || BULLET[i].BulletDirect == 2)
+		printf("│ ");
+	else
+		printf("ㅡ");
+	setColor(15, 0);
+
+}
+void Gun3Print(int i) {
+
+	setColor(4, 0);
+	printf("::");
+	setColor(15, 0);
+
+}
+void Gun4Print(int i) {
+
+	setColor(12, 0);
+	printf("▩");
+	setColor(15, 0);
+
+}
+void mine(int i) {
+	SetCurrentCursorPos(BULLET[i].x, BULLET[i].y);
+
+	if (DetectBullet_Collision(BULLET[i].x, BULLET[i].y, BULLET[i].Type) == 0) {
+		BULLET[i].UseFlag = 0;
+		return;
+	}
+
+	//Collision_mapset(i, 3);
+
+
+}
+int DetectBullet_Collision(int posX, int posY, int type) {
+
+	int i;
+
+	PC_mapPosition.X = (posX - GBOARD_ORIGIN_X) / 2;
+	PC_mapPosition.Y = posY - GBOARD_ORIGIN_Y;
+
+	if (curMap[PC_mapPosition.Y][PC_mapPosition.X] == 1)
+		return 0;
+
+	else if (curMap[PC_mapPosition.Y][PC_mapPosition.X] == 7) {
+
+		if (type == 1)
+			Boss_Health -= 15;
+		else if (type == 2)
+			Boss_Health -= 35;
+		else if (type == 3)
+			Boss_Health -= 15;
+		else
+			Boss_Health -= 35;
+
+		return 0;
+
+	}
+	else if (curMap[PC_mapPosition.Y][PC_mapPosition.X] == 6)
+	{
+		for (i = 0; i < NPC_MAX; i++)
+		{
+			if (NPC[i].x == posX && NPC[i].y == posY && NPC[i].UseFlag == 1)
+			{
+				if (type == 1)
+					NPC[i].NpcHealth--;
+				else if (type == 2)
+					NPC[i].NpcHealth -= 3;
+				else if (type == 3)
+					NPC[i].NpcHealth -= 1;
+				else
+					NPC[i].NpcHealth -= 3;
+
+				if (NPC[i].NpcHealth <= 0)
+				{
+					NPC[i].UseFlag = 0;
+					NPC_Collision_mapset(i, 0);
+					SetCurrentCursorPos(posX, posY);
+					printf("  ");
+					score += 100;
+					int prob;
+					prob = rand() % 3;
+					if (prob == 0)
+					{
+						NPC_ItemDrop(i, posX, posY);
+					}
+				}
+
+				return 0;
+			}
+		}
+	}
+	return 1;
+}
+
+
+
+
+void DeleteGunPicture(int tmp) {
+
+	for (int i = 0; i < 10; i++)
+	{
+		if (score != StageClear)
+			SetCurrentCursorPos(tmp, 6 + i);
+		else
+			SetCurrentCursorPos(tmp, 6 + i);
+
+		printf("                                    ");
+	}
+}
+void DrawGun()
+{
+	int tmp;
+
+	if (score == StageClear)
+		tmp = 70;
+	else
+		tmp = 50;
+
+	SetCurrentCursorPos(tmp, 5);
+	printf("*******************");
+
+
+	SetCurrentCursorPos(tmp, 16);
+	printf("*******************");
+	switch (PC_GunMode)
+	{
+	case 1:
+		DeleteGunPicture(tmp);
+
+		SetCurrentCursorPos(tmp, 9);
+
+		setColor(6, 0);
+		printf(" ______________           ");
+		setColor(8, 0);
+		SetCurrentCursorPos(tmp, 10);
+		printf(" ■■■■■■");
+		setColor(6, 0);
+		printf("■                  ");
+		setColor(8, 0);
+		SetCurrentCursorPos(tmp, 11);
+		printf("  ■■■                     ");
+		SetCurrentCursorPos(tmp, 12);
+		printf(" ■■                       ");
+		SetCurrentCursorPos(tmp, 13);
+		printf("■■                        ");
+		setColor(15, 0);
+		break;
+	case 2:
+		if (GUN2TRUE == 1)
+		{
+			DeleteGunPicture(tmp);
+			SetCurrentCursorPos(tmp, 9);
+			setColor(8, 0);
+			printf("           ■■■■");
+			SetCurrentCursorPos(tmp, 10);
+			printf(" ■■■■■■■■■■■");
+			setColor(15, 0);
+			printf("========");
+			setColor(8, 0);
+			printf("■");
+			setColor(10, 0);
+			printf("■");
+			setColor(8, 0);
+			SetCurrentCursorPos(tmp, 11);
+			printf(" ■■■   ■■  ■■");
+			SetCurrentCursorPos(tmp, 12);
+			printf(" ■■    ■");
+			setColor(15, 0);
+			break;
+		}
+		break;
+	case 3:
+		if (GUN3TRUE == 1)
+		{
+			DeleteGunPicture(tmp);
+			SetCurrentCursorPos(tmp, 9);
+			setColor(8, 0);
+			printf(" ■■■■■■■■■");
+			setColor(15, 0);
+			printf("===");
+			setColor(8, 0);
+			printf("■");
+			setColor(4, 0);
+			printf("■");
+			setColor(8, 0);
+			SetCurrentCursorPos(tmp, 10);
+			printf(" ■■  ■   ■");
+			SetCurrentCursorPos(tmp, 11);
+			printf("      ■    ■");
+			SetCurrentCursorPos(tmp, 12);
+			printf("              ■");
+			setColor(15, 0);
+			break;
+		}
+		break;
+	case 4:
+		if (GUN4TRUE == 1)
+		{
+			DeleteGunPicture(tmp);
+			SetCurrentCursorPos(tmp, 7);
+			printf("MINE / %d", ammo[2]);
+			setColor(12, 0);
+			SetCurrentCursorPos(tmp, 9);
+			printf("        ■        ");
+			SetCurrentCursorPos(tmp, 10);
+			printf("    ■■■■■");
+			SetCurrentCursorPos(tmp, 11);
+			printf("  ■  ■■■  ■");
+			SetCurrentCursorPos(tmp, 12);
+			printf("■■■■■■■■■");
+			setColor(15, 0);
+			break;
+		}
+		break;
+	case 5:
+		if (GUN5TRUE == 1)
+		{
+			DeleteGunPicture(tmp);
+			SetCurrentCursorPos(tmp, 7);
+			printf("HEALPACK / %d", ammo[3]);
+			SetCurrentCursorPos(tmp, 8);
+			setColor(15, 0);
+			printf("    ■■■");
+			SetCurrentCursorPos(tmp, 9);
+			printf("  ■  ");
+			setColor(4, 0);
+			printf("■  ");
+			setColor(15, 0);
+			printf("■  ");
+			SetCurrentCursorPos(tmp, 10);
+			printf("  ■  ");
+			setColor(4, 0);
+			printf("■  ");
+			setColor(15, 0);
+			printf("■  ");
+			setColor(4, 0);
+			SetCurrentCursorPos(tmp, 11);
+			setColor(15, 0);
+			printf("  ■");
+			setColor(4, 0);
+			printf("■■■");
+			setColor(15, 0);
+			printf("■    ");
+			setColor(15, 0);
+			SetCurrentCursorPos(tmp, 12);
+			printf("  ■  ");
+			setColor(4, 0);
+			printf("■  ");
+			setColor(15, 0);
+			printf("■  ");
+			SetCurrentCursorPos(tmp, 13);
+			printf("  ■  ");
+			setColor(4, 0);
+			printf("■  ");
+			setColor(15, 0);
+			printf("■  ");
+			SetCurrentCursorPos(tmp, 14);
+			printf("    ■■■");
+			setColor(15, 0);
+			break;
+		}
+		break;
+	}
+}
+
+void DrawUI()
+{
+	int tmp;
+	if (score != StageClear) {
+		SetCurrentCursorPos(50, 17);
+		setColor(10, 0);
+		printf("SCORE : ");
+		setColor(15, 0);
+		tmp = 50;
+		SetCurrentCursorPos(58, 17);
+		setColor(10, 0);
+		printf("%d", score);
+		setColor(15, 0);
+	}
+	else
+		tmp = 70;
+
+
+
+	SetCurrentCursorPos(tmp, 19);
+	printf("                          ");
+	SetCurrentCursorPos(tmp, 19);
+	for (int i = 0; i < hp; i++)
+	{
+		setColor(4, 0);
+		printf("♥ ");
+		setColor(15, 0);
+	}
+
+}
+
+
+void CreateNpc(int x, int y) {
+	COORD Npc;
+
+	Npc.X = 2 * x + GBOARD_ORIGIN_X;
+	Npc.Y = y + GBOARD_ORIGIN_Y;
+
+	int i;
+	int n;
+	for (i = 0; i < NPC_MAX; i++) {
+
+		if (NPC[i].UseFlag == 0) {
+
+			NPC[i].UseFlag = 1;
+
+			int n = rand() % 2;
+
+			if (n == 0)
+				NPC[i].Type = 0;
+			else
+				NPC[i].Type = 1;
+
+			NPC[i].x = Npc.X;
+			NPC[i].y = Npc.Y;
+			//NPC[i].NpcDirect = rand() % 4;
+			if (x == 1)
+			{
+				NPC[i].NpcDirect = 1;
+			}
+			else
+			{
+				NPC[i].NpcDirect = 3;
+			}
+
+			if (NPC[i].Type == 0)
+			{
+				NPC[i].NpcHealth = 3;
+			}
+			else if (NPC[i].Type == 1)
+			{
+				NPC[i].NpcHealth = 2;
+			}
+
+			return;
+		}
+	}
+}
+void DrawNPC() {
+
+	int i;
+
+	for (i = 0; i < NPC_MAX; i++) {
+
+		int x = NPC[i].x;
+		int y = NPC[i].y;
+		x /= 2;
+		x *= 2;
+		if (NPC[i].UseFlag == 1) {
+
+			SetCurrentCursorPos(x, y);
+
+			if (NPC[i].Type == 0)
+				setColor(2, 0);
+			else
+				setColor(12, 0);
+
+			if (NPC[i].NpcHealth == 3)
+				setColor(2, 0);
+			else if (NPC[i].NpcHealth == 2)
+				setColor(12, 0);
+			else
+				setColor(4, 0);
+			printf("☎");
+
+			setColor(15, 0);
+
+			NPC_Collision_mapset(i, 6);
+
+		}
+		SetCurrentCursorPos(Pos.X, Pos.Y);
+	}
+}
+void NpcAction(int count) {
+
+	int i;
+	for (i = 0; i < NPC_MAX; i++)
+	{
+		if (NPC[i].UseFlag == 1)
+		{
+			if (NPC[i].Type == 0 || NPC[i].Type == 1)
+			{
+				if (score < 2000)
+				{
+					if (count % 20 == 0)
+						NpcUpdate(i);
+				}
+				else if (score < 5000)
+				{
+					if (count % 15 == 0)
+						NpcUpdate(i);
+				}
+				else
+				{
+					if (count % 10 == 0)
+						NpcUpdate(i);
+				}
+
+			}
+		}
+	}
+}
+void NpcUpdate(int i)//종민수정
+{
+
+	SetCurrentCursorPos(NPC[i].x, NPC[i].y);
+	printf("  ");
+
+	int dirtmp;
+	if (DetectNPC_Collision(NPC[i].x, NPC[i].y, -1) == 2)
+	{
+		dirtmp = rand() % 4;
+		while (DetectNPC_Collision(NPC[i].x, NPC[i].y, dirtmp) == 0 || NPC[i].NpcDirect - dirtmp == 2 || NPC[i].NpcDirect - dirtmp == -2)
+		{
+			dirtmp = rand() % 4;
+		}
+		NPC[i].NpcDirect = dirtmp;
+		if (dirtmp == 0)
+		{
+			NPC_Collision_mapset(i, 0);
+			NPC[i].y--;
+			return 0;
+		}
+		else if (dirtmp == 1)
+		{
+			NPC_Collision_mapset(i, 0);
+			NPC[i].x += 2;
+			return 0;
+		}
+		else if (dirtmp == 2)
+		{
+			NPC_Collision_mapset(i, 0);
+			NPC[i].y++;
+			return 0;
+		}
+		else if (dirtmp == 3)
+		{
+			NPC_Collision_mapset(i, 0);
+			NPC[i].x -= 2;
+			return 0;
+		}
+		return;
+	}
+	else if (NPC[i].NpcDirect == 0)
+	{
+		if (DetectNPC_Collision(NPC[i].x, NPC[i].y, 0) == 0)
+		{
+			dirtmp = rand() % 4;
+			while (DetectNPC_Collision(NPC[i].x, NPC[i].y, dirtmp) == 0)
+			{
+				dirtmp = rand() % 4;
+			}
+			NPC[i].NpcDirect = dirtmp;
+			return;
+		}
+
+		NPC_Collision_mapset(i, 0);
+		NPC[i].y--;
+	}
+	else if (NPC[i].NpcDirect == 1)
+	{
+		if (DetectNPC_Collision(NPC[i].x, NPC[i].y, 1) == 0)
+		{
+			dirtmp = rand() % 4;
+			while (DetectNPC_Collision(NPC[i].x, NPC[i].y, dirtmp) == 0)
+			{
+				dirtmp = rand() % 4;
+			}
+			NPC[i].NpcDirect = dirtmp;
+			return;
+		}
+
+		NPC_Collision_mapset(i, 0);
+		NPC[i].x += 2;
+	}
+	else if (NPC[i].NpcDirect == 2)
+	{
+		if (DetectNPC_Collision(NPC[i].x, NPC[i].y, 2) == 0)
+		{
+			dirtmp = rand() % 4;
+			while (DetectNPC_Collision(NPC[i].x, NPC[i].y, dirtmp) == 0)
+			{
+				dirtmp = rand() % 4;
+			}
+			NPC[i].NpcDirect = dirtmp;
+			return;
+		}
+
+		NPC_Collision_mapset(i, 0);
+		NPC[i].y++;
+
+	}
+	else if (NPC[i].NpcDirect == 3)
+	{
+		if (DetectNPC_Collision(NPC[i].x, NPC[i].y, 3) == 0)
+		{
+			dirtmp = rand() % 4;
+			while (DetectNPC_Collision(NPC[i].x, NPC[i].y, dirtmp) == 0)
+			{
+				dirtmp = rand() % 4;
+			}
+			NPC[i].NpcDirect = dirtmp;
+			return;
+		}
+
+		NPC_Collision_mapset(i, 0);
+		NPC[i].x -= 2;
+	}
+}
+int DetectNPC_Collision(int posX, int posY, int dir) { //종민수정
+	COORD NPC_mapPosition;
+
+	int x, y;
+	x = posX;
+	y = posY;
+	if (dir == 0)
+	{
+		posY--;
+	}
+	else if (dir == 1)
+	{
+		posX += 2;
+	}
+	else if (dir == 2)
+	{
+		posY++;
+
+	}
+	else if (dir == 3)
+	{
+		posX -= 2;
+	}
+
+	NPC_mapPosition.X = (posX - GBOARD_ORIGIN_X) / 2;
+	NPC_mapPosition.Y = posY - GBOARD_ORIGIN_Y;
+	x = (x - GBOARD_ORIGIN_X) / 2;
+	y = (y - GBOARD_ORIGIN_Y);
+
+	if (curMap[NPC_mapPosition.Y][NPC_mapPosition.X] == 1)
+		return 0;
+	else if (curMap[NPC_mapPosition.Y][NPC_mapPosition.X] == 2 && mooj == 0)
+	{
+		hp--;
+		mooj = 1;
+		moojend = (count + 60) % 200;
+		return 0;
+	}
+	else if (map_num == 1)//1번맵
+	{
+		for (int i = 0; i < 64; i++)
+		{
+			if (x == CROSSROAD[i].X && y == CROSSROAD[i].Y)
+			{
+				return 2;
+			}
+		}
+	}
+	else //2번맵
+	{
+		for (int i = 0; i < 49; i++)
+		{
+			if (x == CROSSROAD2[i].X && y == CROSSROAD2[i].Y)
+			{
+				return 2;
+			}
+		}
+	}
+
+	return 1;
+
+}
+
+void NPC_Collision_mapset(int i, int elem) {
+	COORD NPC_mapPosition;
+	NPC_mapPosition.X = (NPC[i].x - GBOARD_ORIGIN_X) / 2;
+	NPC_mapPosition.Y = NPC[i].y - GBOARD_ORIGIN_Y;
+	curMap[NPC_mapPosition.Y][NPC_mapPosition.X] = elem;
+}
+void NPC_ItemDrop(int i, int x, int y)
+{
+	COORD NPC_mapPosition;
+	NPC_mapPosition.X = (NPC[i].x - GBOARD_ORIGIN_X) / 2;
+	NPC_mapPosition.Y = NPC[i].y - GBOARD_ORIGIN_Y;
+
+	int itemtype;
+	itemtype = rand() % 4 + 50;
+	curMap[NPC_mapPosition.Y][NPC_mapPosition.X] = itemtype; ////////////////////
+}
+void PC_GetItem()
+{
+	COORD PC_mapPosition;
+	PC_mapPosition.X = (Pos.X - GBOARD_ORIGIN_X) / 2;
+	PC_mapPosition.Y = Pos.Y - GBOARD_ORIGIN_Y;
+
+	if (curMap[PC_mapPosition.Y + 1][PC_mapPosition.X + 1] >= 50 && curMap[PC_mapPosition.Y + 1][PC_mapPosition.X + 1] <= 53)
+	{
+		if (curMap[PC_mapPosition.Y + 1][PC_mapPosition.X + 1] == 50)
+		{
+			GUN2TRUE = 1;
+			ammo[0] += 10;
+		}
+		else if (curMap[PC_mapPosition.Y + 1][PC_mapPosition.X + 1] == 51)
+		{
+			GUN3TRUE = 1;
+			ammo[1] += 40;
+		}
+		else if (curMap[PC_mapPosition.Y + 1][PC_mapPosition.X + 1] == 52)
+		{
+			GUN4TRUE = 1;
+			ammo[2] += 5;
+		}
+		else if (curMap[PC_mapPosition.Y + 1][PC_mapPosition.X + 1] == 53)
+		{
+			GUN5TRUE = 1;
+			ammo[3] += 1;
+		}
+		curMap[PC_mapPosition.Y + 1][PC_mapPosition.X + 1] = 0;
+	}
+
+}
+
+
+////////////////////////////////////BOSS
+
+
+
+
+
+
+void Boss_Shoot() {// 보스맵의 3번부터 21전까지 x의 홀수 칸마다 총알 만들기 ///종민
+   //int x, y;//흔히 쓰는 i,j와 같은 맥락
+
+	COORD Boss_Bullet;//구초체 변수 선언
+
+	int dir;
+
+	dir = rand() % 4;
+	if (dir == 0)
+	{
+		for (int i = 3; i <= 30; i += 2)
+		{
+			Boss_BulletPos.X = 2 * i + (GBOARD_ORIGIN_X / 2);//아래방향
+			Boss_BulletPos.Y = GBOARD_ORIGIN_Y + 23; //1
+			Boss_Bullet.X = Boss_BulletPos.X;
+			Boss_Bullet.Y = Boss_BulletPos.Y; //구조체 변수에 총의 좌표 저장
+
+			Boss_CreateBullet(Boss_Bullet, 0);
+		}
+	}
+	else if (dir == 1)
+	{
+		for (int i = 1; i < 25; i += 2)
+		{
+			Boss_BulletPos.X = GBOARD_ORIGIN_X + 2;//아래방향
+			Boss_BulletPos.Y = GBOARD_ORIGIN_Y + i; //1
+			Boss_Bullet.X = Boss_BulletPos.X;
+			Boss_Bullet.Y = Boss_BulletPos.Y; //구조체 변수에 총의 좌표 저장
+
+			Boss_CreateBullet(Boss_Bullet, 1);
+		}
+	}
+	else if (dir == 2)
+	{
+		for (int i = 4; i <= 30; i += 2)
+		{
+			Boss_BulletPos.X = 2 * i + (GBOARD_ORIGIN_X / 2);//아래방향
+			Boss_BulletPos.Y = GBOARD_ORIGIN_Y + 1; //1
+			Boss_Bullet.X = Boss_BulletPos.X;
+			Boss_Bullet.Y = Boss_BulletPos.Y; //구조체 변수에 총의 좌표 저장
+
+			Boss_CreateBullet(Boss_Bullet, 2);
+		}
+	}
+	else if (dir == 3)
+	{
+		for (int i = 2; i < 24; i += 2)
+		{
+			Boss_BulletPos.X = GBOARD_ORIGIN_X + 56;//아래방향
+			Boss_BulletPos.Y = GBOARD_ORIGIN_Y + i; //1
+			Boss_Bullet.X = Boss_BulletPos.X;
+			Boss_Bullet.Y = Boss_BulletPos.Y; //구조체 변수에 총의 좌표 저장
+
+			Boss_CreateBullet(Boss_Bullet, 3);
+		}
+	}
+}
+
+void Boss_CreateBullet(COORD Boss_Bullet, int direction) {
+
+	int i;
+	for (i = 0; i < BOSS_MAX; i++) {
+
+		if (BOSS[i].UseFlag == 0) {//useflag가 0이고 맵의 위치의 값이 1이 아니면 = 벽과 충돌이 아니라면
+
+			BOSS[i].UseFlag = 1;//useflag를 1
+			BOSS[i].x = Boss_Bullet.X;//
+			BOSS[i].y = Boss_Bullet.Y;//좌표 저장
+			BOSS[i].BossDirect = direction;//총알 방향은 아래
+			return;
+		}
+	}
+}
+
+void Boss_ShotAction() {
+
+	int i;
+	for (i = 0; i < BOSS_MAX; i++) {
+
+		if (BOSS[i].UseFlag == 1) {//총알의 useflag가 1이면
+			SetCurrentCursorPos(BOSS[i].x, BOSS[i].y);//총알 좌표 등록
+			printf("  ");//지우기
+			//방향마다 
+
+			if (BOSS[i].BossDirect == 0) {// 아래yyyy
+
+				if (DetectBossBullet_Collision(BOSS[i].x, BOSS[i].y - 1) == 0) {
+					BOSS[i].UseFlag = 0;//충돌검사
+					//return;
+				}
+				else if (DetectBossBullet_Collision(BOSS[i].x, BOSS[i].y) == 2) {
+					hp--;
+					BOSS[i].UseFlag = 0;
+				}
+
+				else
+					BOSS[i].y -= 1;//위로 한칸이동
+
+			}
+
+			else if (BOSS[i].BossDirect == 1) {// 왼 -> 오른
+
+				if (DetectBossBullet_Collision(BOSS[i].x + 2, BOSS[i].y) == 0) {
+					BOSS[i].UseFlag = 0;
+
+					//return;
+				}
+				else if (DetectBossBullet_Collision(BOSS[i].x, BOSS[i].y) == 2) {
+
+					hp--;
+					BOSS[i].UseFlag = 0;
+				}
+
+				else
+					BOSS[i].x += 2;//오른쪽으로 한칸이동
+
+			}
+
+			else if (BOSS[i].BossDirect == 2) {// 위
+
+				if (DetectBossBullet_Collision(BOSS[i].x, BOSS[i].y + 1) == 0) {
+					BOSS[i].UseFlag = 0;
+
+					//return;
+				}
+				else if (DetectBossBullet_Collision(BOSS[i].x, BOSS[i].y) == 2) {
+					hp--;
+					BOSS[i].UseFlag = 0;
+				}
+
+				else
+					BOSS[i].y++;//아래로 한칸이동
+
+			}
+
+			else if (BOSS[i].BossDirect == 3) {// 오른->왼
+
+				if (DetectBossBullet_Collision(BOSS[i].x - 2, BOSS[i].y) == 0) {
+					BOSS[i].UseFlag = 0;
+
+					//return;
+				}
+				else if (DetectBossBullet_Collision(BOSS[i].x, BOSS[i].y) == 2) {
+					hp--;
+					BOSS[i].UseFlag = 0;
+				}
+
+				else
+					BOSS[i].x -= 2;//왼쪽으로 이동
+
+			}
+		}
+	}
+}
+
+
+
+
+void Boss_DrawShot() {
+
+	int i;
+
+	for (i = 0; i < BOSS_MAX; i++) {
+
+		int x = BOSS[i].x;
+		int y = BOSS[i].y;
+		if (BOSS[i].UseFlag == 1) {
+
+			SetCurrentCursorPos(x, y);
+			printf("☆");//별모양 출력
+
+		}
+		SetCurrentCursorPos(Boss_Pos.X, Boss_Pos.Y);
+	}
+
+
+}
+
+//////////BOSS HP ///////////
+
+void DoProgress(char label[], int step, int total)
+{
+	//progress width
+	const int pwidth = 72;
+
+	//minus label len
+	int width = pwidth - strlen(label);
+	int pos = (step * width) / total;
+
+
+	int percent = (step * 100) / total;
+
+	//set green text color, only on Windows
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_GREEN);
+	SetCurrentCursorPos(2, 3);
+	printf("%s[", label);
+
+	//fill progress bar with =
+	for (int i = 0; i < pos; i++)
+		printf("%c", '=');
+
+	//fill progress bar with spaces
+	printf("% *c", width - pos + 1, ']');
+	printf(" %3d%%\r", percent);
+
+	setColor(15, 0);
+	//reset text color, only on Windows
+	//SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 0x08);
+}
+
+void DrawBoss(char blockInfo[5][8]) {
+
+	int x, y;
+	COORD Boss_mapPosition;
+	COORD curPos = Boss_Pos;
+
+	for (y = 0; y < 5; y++) {
+
+		for (x = 0; x < 8; x++) {
+			SetCurrentCursorPos(curPos.X + (x * 2), curPos.Y + y);
+			Boss_mapPosition.X = (curPos.X - GBOARD_ORIGIN_X) / 2;
+			Boss_mapPosition.Y = curPos.Y - GBOARD_ORIGIN_Y;
+
+			if (blockInfo[y][x] == 7) {
+				curMap[Boss_mapPosition.Y + y][Boss_mapPosition.X + x] = 7;
+				printf("■");
+			}
+			else if (blockInfo[y][x] == 8) {
+				curMap[Boss_mapPosition.Y + y][Boss_mapPosition.X + x] = 8;
+				printf("@@");
+			}
+			else if (blockInfo[y][x] == 0) {
+				curMap[Boss_mapPosition.Y + y][Boss_mapPosition.X + x] = 0;
+				printf("  ");
+			}
+			else if (blockInfo[y][x] == 9) {
+				curMap[Boss_mapPosition.Y + y][Boss_mapPosition.X + x] = 9;
+				printf("--");
+			}
+			else if (blockInfo[y][x] == 10) {
+				curMap[Boss_mapPosition.Y + y][Boss_mapPosition.X + x] = 10;
+				printf("＼");
+			}
+			else {
+				curMap[Boss_mapPosition.Y + y][Boss_mapPosition.X + x] = 11;
+				printf("／");
+			}
+
+		}
+	}
+
+
+	SetCurrentCursorPos(curPos.X, curPos.Y);
+}
+
+int DetectBossBullet_Collision(int posX, int posY) {
+
+
+	Boss_mapPosition.X = (posX - GBOARD_ORIGIN_X) / 2;
+	Boss_mapPosition.Y = posY - GBOARD_ORIGIN_Y;
+
+	if (curMap[Boss_mapPosition.Y][Boss_mapPosition.X] == 1)
+		return 0;
+	else if (curMap[Boss_mapPosition.Y][Boss_mapPosition.X] == 2)
+		return 2;
+	return 1;
+}
+void Boss_DeleteBlock(char blockInfo[5][8]) {
+
+	int x, y;
+	COORD curPos = Boss_Pos;
+
+
+	Boss_mapPosition.X = (curPos.X - GBOARD_ORIGIN_X) / 2;
+	Boss_mapPosition.Y = curPos.Y - GBOARD_ORIGIN_Y;
+
+	for (y = 0; y < 5; y++) {
+		for (x = 0; x < 8; x++) {
+
+			Boss_mapPosition.X = (curPos.X - GBOARD_ORIGIN_X) / 2;
+			Boss_mapPosition.Y = curPos.Y - GBOARD_ORIGIN_Y;
+
+			SetCurrentCursorPos(curPos.X + x * 2, curPos.Y + y);
+
+			if (blockInfo[y][x] == 7 || blockInfo[y][x] == 8 || blockInfo[y][x] == 9 || blockInfo[y][x] == 10 || blockInfo[y][x] == 11 || blockInfo[y][x] == 0) {
+				printf("  ");
+				char Boss_Model[5][8]
+					= {
+
+					   {0,0,7,7,7,7,0,0},
+					   {0,7,0,10,11,0,7,0},
+					   {7,0,8,0,0,8,0,7},
+					   {0,7,0,9,9,0,7,0},
+					   {0,0,7,7,7,7,0,0},
+
+				};
+				if (blockInfo[y][x] == 7)
+					curMap[Boss_mapPosition.Y][Boss_mapPosition.X] == 7;
+				else if (blockInfo[y][x] == 8)
+					curMap[Boss_mapPosition.Y][Boss_mapPosition.X] == 8;
+				else if (blockInfo[y][x] == 9)
+					curMap[Boss_mapPosition.Y][Boss_mapPosition.X] == 9;
+				else if (blockInfo[y][x] == 10)
+					curMap[Boss_mapPosition.Y][Boss_mapPosition.X] == 10;
+				else if (blockInfo[y][x] == 11)
+					curMap[Boss_mapPosition.Y][Boss_mapPosition.X] == 11;
+				else
+					curMap[Boss_mapPosition.Y][Boss_mapPosition.X] == 0;
+			}
+		}
+	}
+	SetCurrentCursorPos(curPos.X, curPos.Y);
+
+
+}
+void Boss_ShiftRight() {
+
+
+	if (DetectBossBullet_Collision(Boss_Pos.X + 16, Boss_Pos.Y) == 0) {
+		Boss_Direction = 1;
+		return;
+	}
+
+	Boss_DeleteBlock(Boss_Model);
+
+
+	Boss_Pos.X += 2;
+	DrawBoss(Boss_Model);
+
+}
+void Boss_ShiftLeft() {
+
+
+	if (DetectBossBullet_Collision(Boss_Pos.X - 2, Boss_Pos.Y) == 0) {
+		Boss_Direction = 0;
+		return;
+	}
+
+	Boss_DeleteBlock(Boss_Model);
+
+
+	Boss_Pos.X -= 2;
+	DrawBoss(Boss_Model);
+
+}
